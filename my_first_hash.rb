96a1deb0d1f9c64a568b84a8974e6c9a8a6ba997
Larissa S <larissafschiavo@gmail.com>
2018-01-09 05:30:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,8 +36,8 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-  shipping_manifest["gun powder"] = 4 
+  shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
